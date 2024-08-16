5f65d67a0d6409c0da09ed3454905b4cded5e4a2
Timo Früh <dev@tifrueh.ch>
2024-08-16 21:58:53 +0200

    Display a help message instead of argc

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void print_help(char argv_0[]) {
+
+    char help_string[] =
+        "Usage:\n"
+        "  %1$s [ -h | --help ]\n"
+        "  %1$s [ -v | --version]\n"
+        "  %1$s <command> ...\n"
+        "\n"
+        "For more information, please consult the manual page.\n";
+
+    printf(help_string, argv_0);
+}
+
 int main(int argc, char* argv[]) {
-    printf("%i", argc);
+    print_help(argv[0]);
     return 0;
 }
