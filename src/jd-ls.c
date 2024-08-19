f3a55e1e7a39354bf6d8f309435cf6b102e5da8e
Timo Früh <dev@tifrueh.ch>
2024-08-19 21:33:53 +0200

    Add help to the ls command

--- src/jd-ls.c
+++ src/jd-ls.c
@@ -1,6 +1,24 @@
 #include "jd-ls.h"
 
+void ls_print_help(char argv_0[]) {
+
+  char help_string[] =
+      "Usage:\n"
+      "  jd %1$s [ -h | --help ]\n"
+      "  jd %1$s [ <jd_path_descriptor> ]\n"
+      "\n"
+      "<jd_path_descriptor> (if specified) must a valid and existing\n"
+      "johnny.decimal area, category or id. To specify an area, use only the\n"
+      "first digit of its categories, for example '2' for area 20-29.\n"
+      "\n"
+      "For more information, please consult the manual page.\n";
+
+  printf(help_string, argv_0);
+}
+
 int jd_ls(int argc, char* argv[]) {
-    printf("subcommand: jd-ls\n");
+
+    ls_print_help(argv[0]);
+
     return SUCCESS;
 }
