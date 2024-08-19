189bbd2e751b13a5e2fecefdad4b2ea4cf557791
Timo Früh <dev@tifrueh.ch>
2024-08-19 21:28:42 +0200

    Add help to the cd command

--- src/jd-cd.c
+++ src/jd-cd.c
@@ -1,6 +1,24 @@
 #include "jd-cd.h"
 
+void cd_print_help(char argv_0[]) {
+
+  char help_string[] =
+      "Usage:\n"
+      "  jd %1$s [ -h | --help ]\n"
+      "  jd %1$s [ <jd_path_descriptor> ]\n"
+      "\n"
+      "<jd_path_descriptor> (if specified) must a valid and existing\n"
+      "johnny.decimal area, category or id. To specify an area, use only the\n"
+      "first digit of its categories, for example '2' for area 20-29. \n"
+      "\n"
+      "For more information, please consult the manual page.\n";
+
+  printf(help_string, argv_0);
+}
+
 int jd_cd(int argc, char* argv[]) {
-    printf("subcommand: jd-cd\n");
+
+    cd_print_help(argv[0]);
+
     return SUCCESS;
 }
